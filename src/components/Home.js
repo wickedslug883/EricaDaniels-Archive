c0a91fc057844173891008246115921e68e26103
darrelwu <darrelwu@utexas.edu>
2023-01-25 18:24:57 -0600

    added class

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,22 @@
 
 const Home = () => {
   return (
-    <div className="home-container">home
-      
+    <div class="home-container" style={{display: "grid", backgroundColor: "#031926"}}>
+       <div class = "landCircle" href="#">
+        <p class="landWelcome" href="#">
+          <span class="test" id ="welcome1">L</span>
+          <span class="test" id ="welcome2">O</span>
+          <span class="test" id ="welcome3">O</span>
+          <span class="test"  id ="welcome4">S</span>
+          <span class="test"  id ="welcome5">E</span>
+          <span class="test"  id ="welcome6">B</span>
+          <span class="test"  id ="welcome7">I</span>
+          <span class="test"  id ="welcome8">T</span>
+          <span class="test"  id ="welcome9">S</span>
+        </p>
+       </div>
     </div>
   )
 }
-
-
 
 export default Home
